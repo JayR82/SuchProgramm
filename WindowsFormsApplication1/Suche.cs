ef81bf203c955b56e875d41621cd59f4f7fdbf54
jay <jay@reutter.de>
2016-09-20 21:39:52 +0200

    hh

--- WindowsFormsApplication1/Suche.cs
+++ WindowsFormsApplication1/Suche.cs
@@ -21,8 +21,6 @@ namespace WindowsFormsApplication1
         public Suche()
         {
             InitializeComponent();
-
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
