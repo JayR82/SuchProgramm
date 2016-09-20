4e3b1a562e4cc1389ab6b07b3f78d4c9694c592b
jay <jay@reutter.de>
2016-09-20 21:08:49 +0200

    gg

---
+++ WindowsFormsApplication1/Suche.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Suche
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.lbInitFolder = new System.Windows.Forms.TextBox();
+            this.dgFoundFiles = new System.Windows.Forms.DataGridView();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            this.lbSuchText = new System.Windows.Forms.TextBox();
+            this.btnSuche = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.dgFoundFiles)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbInitFolder
+            // 
+            this.lbInitFolder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbInitFolder.Enabled = false;
+            this.lbInitFolder.Location = new System.Drawing.Point(83, 3);
+            this.lbInitFolder.Name = "lbInitFolder";
+            this.lbInitFolder.Size = new System.Drawing.Size(756, 20);
+            this.lbInitFolder.TabIndex = 1;
+            this.lbInitFolder.Text = "Initialer Ordner zur Suche...";
+            this.lbInitFolder.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dgFoundFiles
+            // 
+            this.dgFoundFiles.AllowDrop = true;
+            this.dgFoundFiles.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.dgFoundFiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgFoundFiles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgFoundFiles.Location = new System.Drawing.Point(83, 63);
+            this.dgFoundFiles.Name = "dgFoundFiles";
+            this.dgFoundFiles.ReadOnly = true;
+            this.dgFoundFiles.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dgFoundFiles.Size = new System.Drawing.Size(756, 276);
+            this.dgFoundFiles.TabIndex = 5;
+            this.dgFoundFiles.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgFoundFiles_CellContentDoubleClick);
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Location = new System.Drawing.Point(845, 3);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(41, 23);
+            this.btnOpenFolder.TabIndex = 2;
+            this.btnOpenFolder.Text = "...";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            this.btnOpenFolder.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbSuchText
+            // 
+            this.lbSuchText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbSuchText.Location = new System.Drawing.Point(83, 33);
+            this.lbSuchText.Name = "lbSuchText";
+            this.lbSuchText.Size = new System.Drawing.Size(756, 20);
+            this.lbSuchText.TabIndex = 3;
+            this.lbSuchText.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lbSuchText_MouseClick);
+            this.lbSuchText.TextChanged += new System.EventHandler(this.SuchText_TextChanged);
+            // 
+            // btnSuche
+            // 
+            this.btnSuche.Location = new System.Drawing.Point(845, 33);
+            this.btnSuche.Name = "btnSuche";
+            this.btnSuche.Size = new System.Drawing.Size(75, 23);
+            this.btnSuche.TabIndex = 4;
+            this.btnSuche.Text = "Suche";
+            this.btnSuche.UseVisualStyleBackColor = true;
+            this.btnSuche.Click += new System.EventHandler(this.btnSuche_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Ordner";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Suchtext";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Ergebnisliste:";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.btnOpenFolder, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btnSuche, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.dgFoundFiles, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lbInitFolder, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lbSuchText, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(942, 342);
+            this.tableLayoutPanel1.TabIndex = 10;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // Suche
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 342);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(400, 200);
+            this.Name = "Suche";
+            this.Text = "Suche";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgFoundFiles)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox lbInitFolder;
+        private System.Windows.Forms.DataGridView dgFoundFiles;
+        private System.Windows.Forms.Button btnOpenFolder;
+        private System.Windows.Forms.TextBox lbSuchText;
+        private System.Windows.Forms.Button btnSuche;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Suche.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Suche : Form
+    {
+        string folder;
+        DataTable dt = new DataTable();
+        
+        string SuchText;
+
+        public Suche()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();
+            folder = fbd.SelectedPath;
+            lbInitFolder.Text = folder;
+            
+        }
+
+        private void SuchText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnSuche_Click(object sender, EventArgs e)
+        {
+            
+            SuchText = lbSuchText.Text;
+            if (SuchText == "")
+            {
+                lbSuchText.Text = "Bitte hier ein Suchtext eingeben!!!";
+            }
+            else
+            {
+                GetAllFiles();
+                dgFoundFiles.DataSource = dt;
+            }
+        }
+
+        private void GetAllFiles()
+        {
+ 	        DataRow dr;
+           
+            // Process the list of files found in the directory.
+            SuchText = "*" + SuchText + "*";
+            string[] s1 = Directory.GetFiles(folder, SuchText, SearchOption.AllDirectories);
+           
+            for (int i = 0; i <= s1.Length - 1; i++)
+            {
+                if (i == 0)
+                {
+                    //Add Data Grid Columns with name
+                    dt.Columns.Add("Name");
+                    dt.Columns.Add("Typ");
+                    dt.Columns.Add("Datum");
+                }
+                //Get each file information
+                FileInfo f = new FileInfo(s1[i]);
+                FileSystemInfo f1 = new FileInfo(s1[i]);
+                dr = dt.NewRow();
+                //Get File name of each file name
+                dr["Name"] = f1.FullName;
+                //Get File Type/Extension of each file 
+                dr["Typ"] = f1.Extension;
+                //Get file Create Date and Time 
+                dr["Datum"] = f1.CreationTime.Date.ToString("dd/MM/yyyy");
+                //Insert collected file details in Datatable
+                dt.Rows.Add(dr);
+            }
+        }
+
+       
+
+        private void dgFoundFiles_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string filepath = (string)dgFoundFiles.Rows[e.RowIndex].Cells[0].Value;
+            if (File.Exists(filepath))
+            {
+                System.Diagnostics.Process.Start(filepath);
+            }
+            else
+            {
+                MessageBox.Show("" + filepath + " kann nicht geöffnet werden!");
+            }
+            
+        }
+
+        private void lbSuchText_MouseClick(object sender, MouseEventArgs e)
+        {
+            lbSuchText.Text = "";
+        }
+
+        
+    }
+}
