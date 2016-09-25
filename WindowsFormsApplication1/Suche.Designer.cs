a5e44b80e40dbf6bb0056df261f2934e92bc910b
jay <jay@reutter.de>
2016-09-25 17:05:32 +0200

    fixed label position
    Signed-off-by: jay <jay@reutter.de>

--- WindowsFormsApplication1/Suche.Designer.cs
+++ WindowsFormsApplication1/Suche.Designer.cs
@@ -109,29 +109,35 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.Size = new System.Drawing.Size(74, 30);
             this.label1.TabIndex = 7;
             this.label1.Text = "Ordner";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label2.Location = new System.Drawing.Point(3, 30);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.Size = new System.Drawing.Size(74, 30);
             this.label2.TabIndex = 8;
             this.label2.Text = "Suchtext";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
             this.label3.Location = new System.Drawing.Point(3, 60);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.Size = new System.Drawing.Size(74, 13);
             this.label3.TabIndex = 9;
-            this.label3.Text = "Ergebnisliste:";
+            this.label3.Text = "Ergebnisliste";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // tableLayoutPanel1
             // 
